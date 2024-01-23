836292276fbb0d76322e558bad58c3e6da0eddbc
Kishore-r-77 <kishorework77@gmail.com>
2024-01-23 15:04:46 +0530

    initial commit

---
+++ backend/classic-kitchen-springboot/src/main/java/com/classickitchen/ClassicKitchenSpringbootApplication.java
@@ -0,0 +1,13 @@
+package com.classickitchen;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClassicKitchenSpringbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClassicKitchenSpringbootApplication.class, args);
+	}
+
+}
---
+++ backend/classic-kitchen-springboot/src/test/java/com/classickitchen/ClassicKitchenSpringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.classickitchen;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClassicKitchenSpringbootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
